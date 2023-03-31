caefc7f3d305aa3d651fb96ab21941c2f232ffdd
Natalia Popova <sverdlova3@gmail.com>
2023-03-31 08:58:36 +0500

    cycle for

---
+++ task4-5-5.js
@@ -0,0 +1,25 @@
+const readline = require('readline');
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+rl.on('line', (input) => {
+    let result;
+    let n = Number(input);
+
+    // -- ваш код начинается тут
+
+    result = 0
+    for (let i = 2; i <= n; i++) {
+        if(i%2==0){
+            result += i;
+        }
+    }
+
+    // -- ваш код заканчивается тут
+
+    console.log(result);
+    rl.close();
+});
