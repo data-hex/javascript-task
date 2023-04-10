a3c7c6cf8fde79af4d44fa78fc991eefe95aea64
Natalia Popova <sverdlova3@gmail.com>
2023-04-10 09:51:00 +0500

    new task

---
+++ task5-9-4.js
@@ -0,0 +1,23 @@
+const readline = require('readline');
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+rl.on('line', (input) => {
+    let result = "";
+    let data = JSON.parse(input);
+
+    // -- ваш код начинается тут
+    data.pop();
+    for(i = 0; i < data.length; i++){
+        result = result.concat(', ', data[i])
+    }
+    result = result.slice(2)
+
+    // -- ваш код заканчивается тут
+
+    console.log(result);
+    rl.close();
+});
